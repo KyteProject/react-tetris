570381da5dd1dc85d7946aa1d16f2124497eaf56
Daniel Smith <headhertz88@gmail.com>
2019-09-02 10:24:39 +0100

    Updates

---
+++ precache-manifest.c4726aaf50a02fe3e9ebcb70e981e8cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233472cb25a184eb9db432f18df9a670",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "4a1148684beda6369ee8",
+    "url": "/react-tetris/static/css/main.bef4d6a0.chunk.css"
+  },
+  {
+    "revision": "52d216385c4526ad0f71",
+    "url": "/react-tetris/static/js/2.aacd3c48.chunk.js"
+  },
+  {
+    "revision": "4a1148684beda6369ee8",
+    "url": "/react-tetris/static/js/main.e51c16e6.chunk.js"
+  },
+  {
+    "revision": "9f8e8f985e23fe474e69",
+    "url": "/react-tetris/static/js/runtime~main.9a7ed037.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
